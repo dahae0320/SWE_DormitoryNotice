08f4472d059bb3ffc88fd9f438b3ae876c094e29
KoreaMango <57595198+KoreaMango@users.noreply.github.com>
2020-11-11 21:52:35 +0900

    재수정

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,26 +22,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         noticeListView = (ListView) findViewById(R.id.noticeListView);
         noticeList = new ArrayList<Notice>();
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02")); noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-        noticeList.add(new Notice("공지사항입니다. ","강민규","2020-11-02"));
-
+        noticeList.add(new Notice(" 2020.2학기 중 생활관실 수시점검 실시 안내","주권희","2020-10-21"));
+        noticeList.add(new Notice(" 2020학년도 2학기 벌점 사항 공지(2차)","주권희","2020-10-17"));
+        noticeList.add(new Notice(" 학생생활관 방역 안내 ","주권희","2020-10-12"));
+        noticeList.add(new Notice(" 2020학년도 2학기 벌점 사항 공지(1차)","주권희","2020-10-8"));
+        noticeList.add(new Notice(" 2019년 냉방종료 및 난방공급 안내)","임병규","2020-09-28"));
+        noticeList.add(new Notice(" (중대본)추석 연휴 생활방역 수칙 ","김경영","2020-09-21"));
+        noticeList.add(new Notice(" 외박신고서 작성 안내 ","학생생활관(가좌) 관리자","2020-09-18"));
 
         adapter = new NoticeListAdapter(getApplicationContext(),noticeList);
         noticeListView.setAdapter(adapter);
+
+
     }
 }
